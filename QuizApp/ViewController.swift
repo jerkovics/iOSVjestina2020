62e4bf5d0e00c83f210a3df084a928d7ce31354e
jerkovics <jerkovich.sanja@gmail.com>
2020-05-09 20:40:00 +0200

    Inital commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Sanja Jerkovic on 5/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
